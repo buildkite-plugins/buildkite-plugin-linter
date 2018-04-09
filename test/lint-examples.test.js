1241f6557081e0e0b8eb96c8c7cd13842d586a90
Tim Lucas <t@toolmantim.com>
2018-04-09 20:34:09 +1000

    Support yaml blocks too

--- test/lint-examples.test.js
+++ test/lint-examples.test.js
@@ -4,6 +4,9 @@ describe('lint-examples', () => {
   describe('valid example', () => {
     it('should be valid')
   })
+  describe('valid example with yaml instead of yml', () => {
+    it('should be valid')
+  })
   describe('invalid example', () => {
     it('should be invalid')
   })
